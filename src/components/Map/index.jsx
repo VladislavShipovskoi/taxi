51e1eef1b45cfff5ee136aeb29bd350f442764c2
Vladislav Shipovskoi <vladislav.shipovskoi@quantumsoft.ru>
2022-12-02 23:00:35 +0700

    added map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import mapboxgl from "mapbox-gl";
+
+import styles from "./styles/index.module.css";
 import '../../App.css';
 
 
 const  Map = () => {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+
+    useEffect(() => {
+        mapboxgl.accessToken = "pk.eyJ1IjoidmxhZGlzbGF2c2hpcG92c2tvaSIsImEiOiJjbGI2Zzh6ZG4wMHRlM3Zud2FoajZlNjZmIn0.wNmAV9uJc9MzK41Z0VYYmw";
+        if (map.current) return;
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [30.3056504, 59.9429126],
+            zoom: 10
+        });
+    });
 
     return (
-        <div>Map</div>
+        <div className={styles.mapWrapper}>
+            <div data-testid="map" className={styles.map} ref={mapContainer} />
+        </div>
     )
 }
 
