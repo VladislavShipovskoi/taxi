1db87cb334a1c09e64c0504ddad8736c0947ee02
Vladislav Shipovskoi <vladislav.shipovskoi@quantumsoft.ru>
2022-12-20 22:21:42 +0700

    add formik and create LoginForm

---
+++ src/components/Login/forms/LoginForm.js
@@ -0,0 +1,75 @@
+import React from  "react";
+import { useFormik } from 'formik';
+import {Link} from "react-router-dom";
+import {TextField} from "@mui/material";
+import {useDispatch} from "react-redux";
+import styles from "../styles/Login.module.css";
+import '../../../App.css';
+import {authenticateRequest} from "../../../features/Auth/actions";
+
+
+export const LoginForm = () => {
+
+    const dispatch = useDispatch();
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: data => {
+            dispatch(authenticateRequest(data))
+        },
+    });
+
+    return (
+        <div className="form-container">
+            <form onSubmit={formik.handleSubmit}>
+                <div className={`form-body ${styles.loginFormBody}`}>
+                    <div className="title">Войти</div>
+
+                    <TextField
+                        fullWidth
+                        required
+                        id="email"
+                        name="email"
+                        label="Email"
+                        data-testid={"email-input"}
+                        type="email"
+                        variant="standard"
+                        margin="normal"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
+                    />
+
+                    <TextField
+                        fullWidth
+                        required
+                        id="password"
+                        name="password"
+                        label="Password"
+                        data-testid={"password-input"}
+                        type="password"
+                        variant="standard"
+                        margin="normal"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+
+                    <div className="forgot-password">Забыли пароль?</div>
+                    <button className={`custom-button ${styles.loginButton} ${(!formik.values.email.length || !formik.values.password.length) && 'disabled'}`} type="submit">Войти</button>
+                    <div className="link-wrapper">
+                        <span>Новый пользователь? </span>
+                        <Link to={"/registration"}>
+                            <span className="link">Регистрация</span>
+                        </Link>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,30 +1,12 @@
-import React, {useState} from "react";
-import {TextField} from "@mui/material";
+import React from "react";
 import {connect} from "react-redux";
-import {Link, Navigate} from "react-router-dom";
-import { authenticateRequest } from "../../features/Auth/actions";
+import {Navigate} from "react-router-dom";
 import {ReactComponent as LoftTaxiLogo} from '../../assets/images/loftTaxiLogo.svg';
-import styles from "./styles/Login.module.css"
 import '../../App.css';
+import {LoginForm} from "./forms/LoginForm";
 
 
-const Login = (props) => {
-    const { authenticate, isLoggedIn } = props
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleSubmitForm = (event) => {
-        event.preventDefault()
-        authenticate(email, password)
-    }
-
-    const handleChangeEmail = (event) => {
-        setEmail(event.target.value)
-    }
-
-    const handleChangePassword = (event) => {
-        setPassword(event.target.value)
-    }
+const Login = ({ isLoggedIn }) => {
 
     return (
         !isLoggedIn ? (
@@ -33,21 +15,7 @@ const Login = (props) => {
                     <LoftTaxiLogo />
                 </div>
                 <div className="right-side">
-                    <form onSubmit={handleSubmitForm} className="form-container">
-                        <div className={`form-body ${styles.loginFormBody}`}>
-                            <div className="title">Войти</div>
-                            <TextField data-testid={"email-input"} required type="email" id="email" value={email} onChange={handleChangeEmail} label="Email" variant="standard" margin="normal" />
-                            <TextField data-testid={"password-input"} required  type="password" id="password" value={password} onChange={handleChangePassword} label="Password" variant="standard" margin="normal" />
-                            <div className="forgot-password">Забыли пароль?</div>
-                            <button className={`custom-button ${(!email.length || !password.length) && 'disabled'}`} type="submit">Войти</button>
-                            <div className="link-wrapper">
-                                <span>Новый пользователь? </span>
-                                <Link to={"/registration"}>
-                                    <span className="link">Регистрация</span>
-                                </Link>
-                            </div>
-                        </div>
-                    </form>
+                    <LoginForm />
                 </div>
             </div>
         ) : (
@@ -60,10 +28,4 @@ const mapStateToProps = (state) => ({
     isLoggedIn: state.auth.isLoggedIn
 })
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        authenticate: (email, password) => dispatch(authenticateRequest({email, password})),
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, null)(Login);
