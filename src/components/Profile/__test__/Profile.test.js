dd164e8a6ceab8985bc2c63169899bc5fa5a0996
Vladislav Shipovskoi <vladislav.shipovskoi@quantumsoft.ru>
2022-12-24 21:31:06 +0700

    fix

--- src/components/Profile/__test__/Profile.test.js
+++ src/components/Profile/__test__/Profile.test.js
@@ -17,26 +17,4 @@ describe("Profile", () => {
             </Router>
         )
     });
-
-    it('correct form', () => {
-        const history = createMemoryHistory();
-
-        renderWithProviders(
-            <Router location={history.location} navigator={history}>
-                <Profile />
-            </Router>
-        )
-
-        const name = screen.getByLabelText(/^Имя владельца/i)
-        const cardNumber = screen.getByLabelText(/^Номер Карты/i)
-        const cardDate = screen.getByLabelText("MM/YY")
-        const cvc = screen.getByLabelText(/^CVC/i)
-        const saveButton = screen.getByRole("button", {name: "Сохранить"});
-
-        expect(name).toBeInTheDocument();
-        expect(cardNumber).toBeInTheDocument();
-        expect(cardDate).toBeInTheDocument();
-        expect(cvc).toBeInTheDocument();
-        expect(saveButton).toBeInTheDocument();
-    });
 });
